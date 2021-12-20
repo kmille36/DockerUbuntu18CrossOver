76ec9d133f3abbdfc6a95207d110637daeffd2f3
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-12-20 09:20:42 +0700

    Update startdockerubuntu1804.sh

--- startdockerubuntu1804.sh
+++ startdockerubuntu1804.sh
@@ -1,4 +1,4 @@
-curl -skLo spinner.sh https://github.com/kmille36/thuonghai/raw/master/katacoda/spinner.sh
+curl -skLo spinner.sh https://raw.githubusercontent.com/kmille36/thuonghai/master/katacoda/spinner.sh
 curl -skLo ubuntu1804 https://raw.githubusercontent.com/kmille36/DockerUbuntu18CrossOver/main/ubuntu1804googleshell.sh 
 chmod +x spinner.sh
 chmod +x ubuntu1804
