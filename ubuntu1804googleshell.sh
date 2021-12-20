6f808717a4bc92d1f5186eca4f5a2f7ce41e0545
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-12-20 09:32:35 +0700

    Update ubuntu1804googleshell.sh

--- ubuntu1804googleshell.sh
+++ ubuntu1804googleshell.sh
@@ -7,3 +7,4 @@ sudo docker exec -i ubuntu18.04 /bin/bash -c "apt update && apt remove -y chromi
 echo "Installing CrossOver (Run Windows Application)..."
 sudo docker exec -i ubuntu18.04 /bin/bash -c "wget https://media.codeweavers.com/pub/crossover/cxlinux/demo/crossover_21.0.0-1.deb && dpkg -i crossover_21.0.0-1.deb ; apt install --assume-yes --fix-broken" > /dev/null 2>&1
 echo "All done! Please click Web Preview > Preview On Port 8080 to access your VM!"
+./spinner.sh sleep 43200
