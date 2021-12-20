99e19594551fea897f7e88060fcca2916a05d377
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-12-20 09:20:14 +0700

    Update startdockerubuntu1804.sh

--- startdockerubuntu1804.sh
+++ startdockerubuntu1804.sh
@@ -1,5 +1,5 @@
-wget -O spinner.sh https://github.com/kmille36/thuonghai/raw/master/katacoda/spinner.sh > /dev/null 2>&1
-wget -O ubuntu1804 https://github.com/thuonghaivn/Developer/raw/main/ubuntu1804googleshell > /dev/null 2>&1
+curl -skLo spinner.sh https://github.com/kmille36/thuonghai/raw/master/katacoda/spinner.sh
+curl -skLo ubuntu1804 https://raw.githubusercontent.com/kmille36/DockerUbuntu18CrossOver/main/ubuntu1804googleshell.sh 
 chmod +x spinner.sh
 chmod +x ubuntu1804
 ./spinner.sh bash ubuntu1804
