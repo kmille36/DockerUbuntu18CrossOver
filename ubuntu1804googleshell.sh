82be4eee18bc036d884369591c345958f768d9f0
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-01-13 16:58:53 +0700

    Update ubuntu1804googleshell.sh

--- ubuntu1804googleshell.sh
+++ ubuntu1804googleshell.sh
@@ -1,5 +1,5 @@
 clear
-echo "Installing Ubuntu 18.04 Environment +  Firefox Web Browser + CrossOver (1-2 minutes)...."
+echo "Installing Ubuntu 18.04 Environment +  Chrome Web Browser + CrossOver (1-2 minutes)...."
 sudo docker run --name ubuntu18.04 --privileged --shm-size 1g -d -p 5900:5900 -p 8080:6080 thuonghai2711/ubuntu-vnc 
 echo "All done! Please click Web Preview > Preview On Port 8080 to access your VM!"
 ./spinner.sh sleep 43200
